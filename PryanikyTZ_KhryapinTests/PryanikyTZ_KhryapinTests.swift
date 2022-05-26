08f25737b5bb97fa380b56c00f8aedd4920aeb56
Vitaly Khryapin <vitkhryapin@gmail.com>
2022-05-26 13:41:08 +0300

    Initial Commit

---
+++ PryanikyTZ_KhryapinTests/PryanikyTZ_KhryapinTests.swift
@@ -0,0 +1,33 @@
+//
+//  PryanikyTZ_KhryapinTests.swift
+//  PryanikyTZ_KhryapinTests
+//
+//  Created by Vitaly Khryapin on 26.05.2022.
+//
+
+import XCTest
+@testable import PryanikyTZ_Khryapin
+
+class PryanikyTZ_KhryapinTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
